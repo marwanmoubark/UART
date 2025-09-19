144c43e9614222871bb7fc8f9ca05c93cac65d63
marwanmoubark <mmarawan098@gmail.com>
2025-09-19 14:33:28 +0300

    Update FSM_RX.v

--- FSM_RX.v
+++ FSM_RX.v
@@ -8,17 +8,20 @@ module FSM_RX #(parameter oversample = 8)
 );
 
 
+    localparam idle  = 3'b000; 
+    localparam start = 3'b001; 
+    localparam data  = 3'b011; 
+    localparam stop  = 3'b010; 
 
+
+    
     reg [2:0] sample_cnt;
     reg [2:0] data_cnt; 
-    reg [7:0] data_reg , data_out_reg;
-    reg [1:0] state;
+    reg [7:0] data_out_reg , data_reg;
+    reg [2:0] C_state , N_state;
  
     
-    localparam idle  = 0; 
-    localparam start = 1; 
-    localparam data  = 2; 
-    localparam stop  = 3; 
+
     
     
     always @(posedge Bclk or negedge reset_n) begin
@@ -26,14 +29,15 @@ module FSM_RX #(parameter oversample = 8)
             data_reg    <= 8'd0;
             sample_cnt  <= 0; 
             data_cnt    <= 0;
-            state       <= idle;
+            N_state       <= idle;
             rx_valid    <= 0;
             data_out_reg    <= 8'd0;
         end
         else begin
             rx_valid <= 1'b0;
+            C_state <= N_state;
             
-            case(state) 
+            case(C_state) 
 
             //////////////////// idle ////////////////////        
             idle: begin
@@ -42,7 +46,7 @@ module FSM_RX #(parameter oversample = 8)
                 data_cnt   <= 0;
                 rx_valid   <= 0 ;
                 if(rx_data == 1'b0) begin  // detect start bit (low)
-                    state <= start;
+                    N_state <= start;
                 end
             end
 
@@ -52,10 +56,10 @@ module FSM_RX #(parameter oversample = 8)
                 // Sample at the middle of start bit
                 if(sample_cnt == (oversample)-1) begin
                     if(rx_data == 1'b0) begin  // Confirm it's still low
-                        state <= data;
+                        N_state <= data;
                     end
                     else begin
-                        state <= idle; // false start bit
+                        N_state <= idle; // false start bit
                     end
                 end
             end
@@ -70,7 +74,7 @@ module FSM_RX #(parameter oversample = 8)
                     data_cnt <= data_cnt + 1;
                     
                     if(data_cnt == 7) begin
-                        state <= stop;
+                        N_state <= stop;
                         sample_cnt <= 0;
                     end
                 end
@@ -85,20 +89,17 @@ module FSM_RX #(parameter oversample = 8)
                         data_out_reg <= data_reg;
                         rx_valid <= 1'b1;
                     end
-                    state <= idle;
+                    N_state <= idle;
                     sample_cnt <= 0;
                     data_cnt <= 0;
                 end
             end
 
-            default: state <= idle;
+            default: N_state <= idle;
             
             endcase
         end
     end
-
-    assign rx_state = state ;
-    assign bit_sample_cnt = sample_cnt;
+    
     assign data_out = data_out_reg;
-    assign current_data = data_reg;
 endmodule
